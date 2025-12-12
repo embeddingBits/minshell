a760d8d6d6da746c8bbf7f6147a4dc42243ab2e7
ebits <tspamiitesh@gmail.com>
2025-12-12 21:17:48 +0530

    Modularise: Modularising all the built-in commands

--- src/main.zig
+++ src/main.zig
@@ -3,71 +3,69 @@ const std = @import("std");
 pub fn main() !void {
 
     while (true) {
-        var read_buffer: [50]u8 = undefined;
+        // Allocator
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        const allocator = arena.allocator();
+
+        // Input commands
+        var read_buffer: [1024]u8 = undefined;
         var reader = std.fs.File.stdin().reader(&read_buffer);
         const stdin = &reader.interface;
 
-        std.debug.print(">> ", .{});
+        std.debug.print("$ ", .{});
 
-        const line = try stdin.takeDelimiterExclusive('\n');
-        var args = std.mem.splitScalar(u8, line, ' ');
+        const input = try stdin.takeDelimiterExclusive('\n');
 
-        const cmds = [_][]const u8{"exit", "type", "echo", "pwd", "ls"};
-
-        if (args.next()) |first| {
-            if (std.mem.eql(u8, first, "exit")) {
-                std.debug.print("goodbye\n", .{});
-                return;
-            } 
-            else if (std.mem.eql(u8, first, "ls")) {
-                var dir = try std.fs.cwd().openDir(".", .{.iterate = true});
-                defer dir.close();
-
-                var iter = dir.iterate();
-
-                while (try iter.next()) |i| {
-                    std.debug.print("{s} ", .{i.name});
-                }
-                std.debug.print("\n", .{});
-
-            } 
-            else if (std.mem.eql(u8, first, "pwd")) {
-                var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-                defer arena.deinit();
-                const alloc = arena.allocator();
-
-                const pwd = try std.fs.cwd().realpathAlloc(alloc, ".");
-                std.debug.print("{s}\n", .{pwd});
-            } 
-            else if (std.mem.eql(u8, first, "echo")) {
-                while(args.next()) |i| {
-                    std.debug.print("{s} ", .{i});
-                }
-                std.debug.print("\n", .{});
-            } 
-            else if (std.mem.eql(u8, first, "type")) {
-                const target = args.next() orelse {
-                    std.debug.print("type: missing argument\n", .{});
-                    continue;
-                };
-
-                var found = false;
-
-                for (cmds) |cmd| {
-                    if (std.mem.eql(u8, target, cmd)) {
-                        std.debug.print("{s} is a builtin\n", .{target});
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
-                    std.debug.print("type: {s} not found\n", .{target});
-                }
-            } else {
-                std.debug.print("{s} is not found\n", .{first});
+        if (input.len > 0) {
+            if (std.mem.eql(u8, input, "exit")) {
+                try exitCommand();
             }
+            try handleCommand(input, allocator);
         }
     }
 }
 
+fn handleCommand(input: []const u8, allocator: std.mem.Allocator) !void {
+    var args_split = std.mem.splitSequence(u8, input, " ");
+    if (args_split.next()) |arg| {
+        if (std.mem.eql(u8, arg, "echo")) {
+            try echoCommand(input);
+        } else if (std.mem.eql(u8, arg, "pwd")) {
+            try pwdCommand(allocator);
+        } else if (std.mem.eql(u8, arg, "exit")) {
+            try exitCommand();
+        }
+    }
+}
+
+fn echoCommand(input: []const u8) !void {
+    const echo_args = input[5..];
+    if (echo_args.len == 0) {
+        std.debug.print("\n", .{});
+    } else {
+        std.debug.print("{s}\n", .{echo_args});
+    }
+}
+
+fn typeCommand(input: []const u8) !void {
+    var args_split = std.mem.splitSequence(u8, input, " ");
+
+    const cmd = [_][]const u8{"exit", "cd", "pwd", "type", "echo"};
+    for (cmd) |builtin| {
+        if (std.mem.eql(u8, args_split.next(), builtin)) {
+            std.debug.print("{s} is a builtin\n", .{builtin});
+        }
+    }
+}
+
+fn pwdCommand(allocator: std.mem.Allocator) !void {
+    const current_directory = try std.fs.cwd().realpathAlloc(allocator, ".");
+    defer allocator.free(current_directory);
+
+    std.debug.print("{s}\n", .{current_directory});
+}
+
+fn exitCommand() !void {
+    std.process.exit(0);
+}
