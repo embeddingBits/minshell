30c41b608102675665c3c95e55f23803fb748b3e
ebits <tspamiitesh@gmail.com>
2025-12-21 23:13:59 +0530

    Logic: Implementing a function for the parsing logic

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Mini experimental shell in Zig from scratch
 
 A rough implementation of a usable shell and then I'll move on to refractoring and structuring of the whole shell
 
-## Things to be done
+## Commands to be done
 - [x] `ls` command
 - [x] `echo` command
 - [x] `exit` command
@@ -14,3 +14,12 @@ A rough implementation of a usable shell and then I'll move on to refractoring a
 - [ ] `dirs` command
 - [ ] `pushd` command
 - [ ] `popd` command
+
+## Features to be implemented
+- [ ] History
+- [ ] History Navigation
+- [ ] Tab Completion
+- [ ] Configuration File
+- [ ] Conditional Statements
+- [ ] Job control `bg`, `fg`, `jobs`
+- [ ] Wildcard characters
--- src/main.zig
+++ src/main.zig
@@ -1,85 +1,93 @@
 const std = @import("std");
 
-var dirStack = std.ArrayListUnmanaged([]u8){};
-const cmd_list = [_][]const u8{"exit", "cd", "pwd", "type", "echo", "builtin", "clear", "dirs", "pushd", "popd"};
+const cmd_list = [_][]const u8{
+    "exit", "cd", "pwd", "type", "echo",
+    "builtin", "clear", "dirs", "pushd", "popd",
+};
 
-const status = 0;
+const ParsedCommand = struct {
+    command: []const u8,
+    argv: []const []const u8,
+};
 
 pub fn main() !void {
-
-    const initCwd = try std.fs.cwd().realpathAlloc(std.heap.page_allocator, ".");
-    try dirStack.append(std.heap.page_allocator, initCwd);
-
     while (true) {
-        // Allocator
         var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
         defer arena.deinit();
         const allocator = arena.allocator();
 
-        // Input commands
+        // Input reader
         var read_buffer: [1024]u8 = undefined;
         var reader = std.fs.File.stdin().reader(&read_buffer);
         const stdin = &reader.interface;
 
-        // Username and Hostname
-        const user = std.posix.getenv("USER");
+        // To display prompt
+        const user = std.posix.getenv("USER") orelse "user";
         var hostname_buf: [64]u8 = undefined;
         const hostname = try std.posix.gethostname(&hostname_buf);
 
-        // Directory Display
-        const cwdPath = try std.fs.cwd().realpathAlloc(allocator, ".");
-        const dirName = std.fs.path.basename(cwdPath);
+        const cwd_path = try std.fs.cwd().realpathAlloc(allocator, ".");
+        const dir_name = std.fs.path.basename(cwd_path);
 
-        std.debug.print("[{?s}@{s} {s}]$ ", .{user, hostname, dirName});
+        std.debug.print("[{s}@{s} {s}]$ ", .{ user, hostname, dir_name });
 
-        const input = try stdin.takeDelimiterExclusive('\n');
+        const input = stdin.takeDelimiterExclusive('\n') catch continue;
+        if (input.len == 0) continue;
 
-        if (input.len > 0) {
-            if (std.mem.eql(u8, input, "exit")) {
-                try exitCommand();
-            }
-            try handleCommand(input, allocator);
-        }
+        try handleCommand(input, allocator);
     }
 }
 
+fn parseCommand(input: []const u8, allocator: std.mem.Allocator) !ParsedCommand {
+    var parts = std.mem.splitSequence(u8, input, " ");
+
+    var argvList = std.ArrayListUnmanaged([]const u8){};
+    errdefer argvList.deinit(allocator);
+
+    while (parts.next()) |part| {
+        if (part.len == 0) continue;
+        try argvList.append(allocator, part);
+    }
+
+    if (argvList.items.len == 0)
+        return error.EmptyCommand;
+
+    const argv = try argvList.toOwnedSlice(allocator);
+
+    return ParsedCommand{
+        .command = argv[0],
+        .argv = argv,
+    };
+}
+
 fn handleCommand(input: []const u8, allocator: std.mem.Allocator) !void {
-    var args_split = std.mem.splitSequence(u8, input, " ");
-    const command = args_split.next() orelse return;
+    const parsed = parseCommand(input, allocator) catch |err| {
+        if (err == error.EmptyCommand) return;
+        return err;
+    };
+    defer allocator.free(parsed.argv);
 
-    if (std.mem.eql(u8, command, "echo")) {
-        try echoCommand(input);
-    } else if (std.mem.eql(u8, command, "pwd")) {
+    const cmd = parsed.command;
+
+    if (std.mem.eql(u8, cmd, "exit")) {
+        exitCommand();
+    } else if (std.mem.eql(u8, cmd, "echo")) {
+        echoCommand(parsed.argv);
+    } else if (std.mem.eql(u8, cmd, "pwd")) {
         try pwdCommand(allocator);
-    } else if (std.mem.eql(u8, command, "exit")) {
-        try exitCommand();
-    } else if (std.mem.eql(u8, command, "clear")) {
-        try clearCommand();
-    } else if (std.mem.eql(u8, command, "true")) {
-        trueCommand();
-    } else if (std.mem.eql(u8, command, "false")) {
-        falseCommand();
-    } else if (std.mem.eql(u8, command, "cd")) {
-        // Trim whitespace
-        const arg = std.mem.trim(u8, args_split.rest(), &std.ascii.whitespace);
-        try cdCommand(arg);
-    } else if (std.mem.eql(u8, command, "builtin")) {
-        try builtinCommand();
+    } else if (std.mem.eql(u8, cmd, "clear")) {
+        clearCommand();
+    } else if (std.mem.eql(u8, cmd, "cd")) {
+        const target = if (parsed.argv.len > 1) parsed.argv[1] else "";
+        try cdCommand(target);
+    } else if (std.mem.eql(u8, cmd, "builtin")) {
+        builtinCommand();
+    } else if (std.mem.eql(u8, cmd, "type")) {
+        typeCommand(parsed.argv);
     } else {
-        var argv_list = std.ArrayListUnmanaged([]const u8){};
-        defer argv_list.deinit(allocator);
-
-        var it = std.mem.splitSequence(u8, input, " ");
-        while (it.next()) |part| {
-            try argv_list.append(allocator, part);
-        }
-
-        const argv = try argv_list.toOwnedSlice(allocator);
-        defer allocator.free(argv);
-
-        executeCommand(allocator, argv) catch |err| {
+        executeCommand(allocator, parsed.argv) catch |err| {
             switch (err) {
-                error.FileNotFound => {}, // already handled inside
+                error.FileNotFound => {},
                 error.AccessDenied => {},
                 else => std.debug.print("exec error: {s}\n", .{@errorName(err)}),
             }
@@ -87,97 +95,89 @@ fn handleCommand(input: []const u8, allocator: std.mem.Allocator) !void {
     }
 }
 
-fn echoCommand(input: []const u8) !void {
-    const echo_args = input[5..];
-    if (echo_args.len == 0) {
+fn echoCommand(argv: []const []const u8) void {
+    if (argv.len <= 1) {
         std.debug.print("\n", .{});
-    } else {
-        std.debug.print("{s}\n", .{echo_args});
-    }
-}
-
-fn typeCommand(input: []const u8) !void {
-    var args_split = std.mem.splitSequence(u8, input, " ");
-
-    for (cmd_list) |builtin| {
-        if (std.mem.eql(u8, args_split.next(), builtin)) {
-            std.debug.print("{s} is a builtin\n", .{builtin});
-        }
-    }
-}
-
-fn pwdCommand(allocator: std.mem.Allocator) !void {
-    const current_directory = try std.fs.cwd().realpathAlloc(allocator, ".");
-    defer allocator.free(current_directory);
-
-    std.debug.print("{s}\n", .{current_directory});
-}
-
-fn builtinCommand() !void {
-    for (cmd_list) |commands| {
-        std.debug.print("{s}\n", .{commands});
-    }
-}
-
-fn clearCommand() !void {
-    const clear_screen = "\x1B[2J\x1B[H";
-    std.debug.print("{s}", .{clear_screen});
-}
-
-fn cdCommand(target: []const u8) !void {
-    const path = if (target.len == 0 or std.mem.eql(u8, target, "~") or std.mem.eql(u8, target, "~/"))
-        std.posix.getenv("HOME") orelse {
-            std.debug.print("cd: HOME environment variable not set\n", .{});
-            return error.HomeNotSet;
-        }
-        else
-        target;
-
-    std.posix.chdir(path) catch |err| {
-        std.debug.print("cd: {s}: No such file or directory\n", .{path});
-        return err;
-    };
-}
-
-fn executeCommand(allocator: std.mem.Allocator, argv: []const []const u8) !void {
-    if (argv.len == 0) {
-        std.debug.print("exec: no command\n", .{});
         return;
     }
 
+    for (argv[1..], 0..) |arg, i| {
+        if (i > 0) std.debug.print(" ", .{});
+        std.debug.print("{s}", .{arg});
+    }
+    std.debug.print("\n", .{});
+}
+
+fn pwdCommand(allocator: std.mem.Allocator) !void {
+    const cwd = try std.fs.cwd().realpathAlloc(allocator, ".");
+    defer allocator.free(cwd);
+    std.debug.print("{s}\n", .{cwd});
+}
+
+fn clearCommand() void {
+    std.debug.print("\x1B[2J\x1B[H", .{});
+}
+
+fn cdCommand(target: []const u8) !void {
+    const path =
+        if (target.len == 0 or std.mem.eql(u8, target, "~"))
+            std.posix.getenv("HOME") orelse {
+                std.debug.print("cd: HOME not set\n", .{});
+                return error.HomeNotSet;
+            }
+        else
+            target;
+
+    std.posix.chdir(path) catch {
+        std.debug.print("cd: {s}: No such directory\n", .{path});
+    };
+}
+
+fn builtinCommand() void {
+    for (cmd_list) |cmd| {
+        std.debug.print("{s}\n", .{cmd});
+    }
+}
+
+fn typeCommand(argv: []const []const u8) void {
+    if (argv.len < 2) return;
+
+    for (cmd_list) |builtin| {
+        if (std.mem.eql(u8, argv[1], builtin)) {
+            std.debug.print("{s} is a builtin\n", .{builtin});
+            return;
+        }
+    }
+
+    std.debug.print("{s} not found\n", .{argv[1]});
+}
+
+
+fn executeCommand(allocator: std.mem.Allocator, argv: []const []const u8) !void {
     const cmd = argv[0];
 
-    // Direct path if it contains '/'
+    // Direct path
     if (std.mem.indexOfScalar(u8, cmd, '/')) |_| {
         try spawnAndWait(allocator, argv);
         return;
     }
 
-    // Search in PATH
-    const path = std.posix.getenv("PATH") orelse "/bin:/usr/bin:/usr/local/bin";
+    const path = std.posix.getenv("PATH") orelse "/bin:/usr/bin";
     var dirs = std.mem.splitScalar(u8, path, ':');
 
     while (dirs.next()) |dir| {
-        const full_path = std.fs.path.join(allocator, &.{dir, cmd}) catch continue;
-        defer allocator.free(full_path);
+        const full = std.fs.path.join(allocator, &.{ dir, cmd }) catch continue;
+        defer allocator.free(full);
 
         if (spawnAndWait(allocator, argv)) {
             return;
-        } else |err| switch (err) {
-            error.FileNotFound => continue,
-            error.AccessDenied => {
-                std.debug.print("{s}: permission denied\n", .{cmd});
-                return;
-            },
-            else => continue,
-        }
+        } else |_| {}
     }
 
     std.debug.print("{s}: command not found\n", .{cmd});
 }
 
 fn spawnAndWait(allocator: std.mem.Allocator, argv: []const []const u8) !void {
-
     var child = std.process.Child.init(argv, allocator);
     child.stdin_behavior = .Ignore;
     child.stdout_behavior = .Inherit;
@@ -187,12 +187,8 @@ fn spawnAndWait(allocator: std.mem.Allocator, argv: []const []const u8) !void {
     _ = try child.wait();
 }
 
-fn trueCommand() bool {
-    status = 0;
-}
-
-fn falseCommand() bool {
-    status = 1;
+fn exitCommand() noreturn {
+    std.process.exit(0);
 }
 
 //fn dirsCommand(allocator: std.mem.Allocator) !void {
@@ -222,7 +218,3 @@ fn falseCommand() bool {
 //
 //    try dirsCommand(allocator);
 //}
-
-fn exitCommand() !void {
-    std.process.exit(0);
-}
